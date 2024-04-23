3086431cb8469d3ea3064ee377aee6c112dbb66c
Yadneya Joshi <61934942+Yadnu@users.noreply.github.com>
2024-04-23 23:38:13 +0530

    Adding backend

---
+++ frontend/src/page/Home.tsx
@@ -0,0 +1,14 @@
+import { Card } from "@/components/ui/card";
+import React from 'react'
+
+type Props = {}
+
+const Home = (props: Props) => {
+  return (
+    <div><Card>
+        </Card>
+        </div>
+  )
+}
+
+export default Home
